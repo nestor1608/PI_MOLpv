6db000c738de1d1d4082cfe4a79b9a6c5313e0ad
nestor1608 <nestor.268@Outlook.com>
2023-03-20 18:26:52 -0300

    update final creo

--- conect.py
+++ conect.py
@@ -3,23 +3,3 @@ import pandas as pd
 def load_data():
   dataset = pd.read_csv('https://objectstorage.sa-saopaulo-1.oraclecloud.com/n/gr4oj5h2hdd0/b/documentos/o/movie_clean.csv')
   return dataset
-
-
-
-dt1 = pd.read_csv('../ratings/1.csv')
-dt2 = pd.read_csv('../ratings/2.csv')
-dt3 = pd.read_csv('../ratings/3.csv')
-dt4 = pd.read_csv('../ratings/4.csv')
-dt5 = pd.read_csv('../ratings/5.csv')
-dt6 = pd.read_csv('../ratings/6.csv')
-dt7 = pd.read_csv('../ratings/7.csv')
-dt8 = pd.read_csv('../ratings/8.csv')
-
-
-
-def data_rating():
-    data=[dt1,dt2,dt3,dt4,dt5,dt6,dt7,dt8,]
-    dataset = pd.concat(data)
-    change_code= {'as943':'as942', 'as5894':'as5087', 'as9037': 'as4402', 'as9059':'as7296', 'as9270' :'as7054', 'ns6706':'ns304','ns7346':'ns160', 'ns8023':'ns1271'}
-    dataset.movieId = dataset.movieId.replace(change_code)
-    return dataset
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from starlette.responses import RedirectResponse
 import pandas as pd
 from conect import load_data
+import json
 
 
 df = load_data()
@@ -14,25 +16,26 @@ async def root():
 
 @app.get('/data')
 async def get_data():
-    return df.to_json(orient="records", lines=True)
+    data = load_data()
+    return JSONResponse(content=json.loads(data.to_json())) 
 
-@app.get('/duration')
+@app.get('/duration/{year}/{platform}/{duration_type}')
 async def get_max_duration(year:int, platform:str, duration_type:str):
     select =df[(df.date_added.dt.year==year) & (df.plataform == platform) & (df.duration_type==duration_type)]
     result = select[select.duration_int== select.duration_int.max()]
     return f'La pelicula con mayor duracion en el año {year} en la plataforma {platform} es {result["title"].values[0]}'  #.to_json(orient="records", lines=True)
 
-@app.get('/score')
+@app.get('/score/{platform}/{scored}/{year}')
 async def get_score_count(platform:str, scored:float, year:int):
     select =df[(df.plataform == platform) & (df.date_added.dt.year==year) &(df.score >= scored)]
     return {'Cantidad': len(select)}
 
-@app.get('/plataform')
+@app.get('/plataform/{platform}')
 async def get_count_platform(platform:str):
     select = df[df.plataform == platform]
     return f'Hay {str(len(select))} peliculas cargadas en la plataforma {platform}' 
 
-@app.get('/actor')
+@app.get('/actor/{platfor}/{year}')
 async def get_actor(platform:str, year:int):
     select = df[(df.plataform == platform) & (df.date_added.dt.year==year)]
     aux= []
@@ -42,6 +45,5 @@ async def get_actor(platform:str, year:int):
             for c in gen:
                 aux.append(c.strip())
     return f'Actor que mas se repite en esta seleccion es {pd.Series(aux).value_counts().index[0]}'
-
 
 
